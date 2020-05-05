3515728ca942f37cf729456f7985d5b9736a4d6f
Álvaro G. Cachón <alvaro.gutierrezcachon@amaris.com>
2020-05-06 00:28:52 +0200

    Fix test.

--- src/test/java/com/amaris/demo/RestAssuredTest.java
+++ src/test/java/com/amaris/demo/RestAssuredTest.java
@@ -9,6 +9,6 @@ public class RestAssuredTest {
 
 	@Test
 	public void exampleOfTestNgMaven() {
-        RestAssured.get("http://test-app-school.2886795276-80-jago05.environments.katacoda.com").then().assertThat().statusCode(200)
+        RestAssured.get("http://test-app-school.2886795276-80-jago05.environments.katacoda.com").then().assertThat().statusCode(200);
 	}
 }
